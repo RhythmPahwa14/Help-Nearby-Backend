f98b10dac3daaf50f131478dc7e0c084fb385bdd
Rhythm Pahwa <rhythmpahwa14@gmail.com>
2026-02-18 21:34:07 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,12 @@ router.post(
     body('name').trim().notEmpty().withMessage('Name is required'),
     body('email').isEmail().withMessage('Please provide a valid email'),
     body('phone').isMobilePhone().withMessage('Please provide a valid phone number'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+    body('password')
+      .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
+      .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+      .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+      .matches(/[0-9]/).withMessage('Password must contain at least one number')
+      .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one special character')
   ],
   register
 );
